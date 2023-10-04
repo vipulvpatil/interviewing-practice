9db96ce957bd492933adb9aeee3e28e192708476
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-10-04 21:30:29 +0530

    added convex hull algorithm

--- Sept2023/golang/convex_hull/hull/convex_hull.go
+++ Sept2023/golang/convex_hull/hull/convex_hull.go
@@ -1,12 +1,105 @@
 package hull
 
-import "github.com/vipulvpatil/interviewing-practice/Sept2023/golang/convex_hull/point"
+import (
+	"math"
+
+	"github.com/vipulvpatil/interviewing-practice/Sept2023/golang/convex_hull/point"
+	"golang.org/x/exp/slices"
+)
 
 func GetConvexHull(points []point.Point) []point.Point {
-	// findLowestPoint(points)
-	// sort point by angle from lowest point
-	// go over points and track last current and next point
-	// if CCW, keep current
-	//
-	return points[0:4]
+	if len(points) < 3 {
+		return points
+	}
+	sortByY(points)
+	lowestPoint := points[0]
+	sortByAngleFromGivenPoint(points[1:], lowestPoint)
+	hullPoints := []point.Point{}
+	hullPoints = append(hullPoints, points[0])
+	hullPoints = append(hullPoints, points[1])
+	for i := 2; i < len(points); {
+		hullPointsLen := len(hullPoints)
+		aPoint := hullPoints[hullPointsLen-2]
+		bPoint := hullPoints[hullPointsLen-1]
+		cPoint := points[i]
+		if !isCCWTurn(aPoint, bPoint, cPoint) {
+			hullPoints = hullPoints[:hullPointsLen-1]
+		} else {
+			hullPoints = append(hullPoints, cPoint)
+			i++
+		}
+	}
+	return hullPoints
+}
+
+func isCCWTurn(a, b, c point.Point) bool {
+	return a.GetX()*(b.GetY()-c.GetY())+b.GetX()*(c.GetY()-a.GetY())+c.GetX()*(a.GetY()-b.GetY()) > 0
+}
+
+func sortByY(points []point.Point) {
+	slices.SortFunc[[]point.Point](points, func(a point.Point, b point.Point) int {
+		if a.GetY() < b.GetY() {
+			return -1
+		}
+		if a.GetY() > b.GetY() {
+			return 1
+		}
+		if a.GetX() > b.GetX() {
+			return -1
+		}
+		if a.GetX() < b.GetX() {
+			return 1
+		}
+		return 0
+	})
+}
+
+func sortByAngleFromGivenPoint(points []point.Point, givenPoint point.Point) {
+	slices.SortFunc[[]point.Point](points, func(a point.Point, b point.Point) int {
+		slopeA := math.Inf(1)
+		slopeB := math.Inf(1)
+		if a.GetX() != givenPoint.GetX() {
+			slopeA = (a.GetY() - givenPoint.GetY()) / (a.GetX() - givenPoint.GetX())
+		}
+		if b.GetX() != givenPoint.GetX() {
+			slopeB = (b.GetY() - givenPoint.GetY()) / (b.GetX() - givenPoint.GetX())
+		}
+
+		if slopeA == slopeB {
+			distASqr := math.Pow(a.GetX()-givenPoint.GetX(), 2) + math.Pow(a.GetY()-givenPoint.GetY(), 2)
+			distBSqr := math.Pow(b.GetX()-givenPoint.GetX(), 2) + math.Pow(b.GetY()-givenPoint.GetY(), 2)
+			if distASqr < distBSqr {
+				return -1
+			} else if distASqr > distBSqr {
+				return 1
+			}
+			return 0
+		}
+
+		if math.IsInf(slopeA, 1) {
+			if slopeB > 0 {
+				return 1
+			}
+			return -1
+		}
+
+		if slopeA > 0 && slopeB > 0 {
+			if slopeA > slopeB {
+				return 1
+			}
+			return -1
+		}
+
+		if slopeA < 0 && slopeB < 0 {
+			if slopeA > slopeB {
+				return 1
+			}
+			return -1
+		}
+
+		if slopeA > 0 && slopeB < 0 {
+			return -1
+		}
+		return 1
+	})
 }
