829cca393d841a848d3fcff6581c3423eb9f7815
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-10-05 15:25:03 +0530

    added merge sort

--- Sept2023/golang/sorts/merge/merge_sort.go
+++ Sept2023/golang/sorts/merge/merge_sort.go
@@ -7,18 +7,44 @@ import (
 type MergeSorter struct{}
 
 func (s *MergeSorter) Sort(arr sortable.SortableOutOfPlace) {
-	for i := 1; i < arr.Len(); i++ {
-		for j := i; j > 0; j-- {
-			if arr.Less(j, j-1) {
-				// arr.Swap(j, j-1)
-			} else {
-				break
-			}
-		}
-	}
+	MergeSort(arr, 0, arr.Len()-1)
 }
 
-// func MergeSort(arr)
+func MergeSort(arr sortable.SortableOutOfPlace, start, end int) {
+	if end-start < 1 {
+		return
+	}
+	mid := start + (end-start)/2
+	tempArray := []any{}
+	MergeSort(arr, start, mid)
+	MergeSort(arr, mid+1, end)
+	i := start
+	j := mid + 1
+	for i <= mid && j <= end {
+		if arr.Less(i, j) {
+			tempArray = append(tempArray, arr.Get(i))
+			i++
+		} else {
+			tempArray = append(tempArray, arr.Get(j))
+			j++
+		}
+	}
+	if i > mid {
+		for j <= end {
+			tempArray = append(tempArray, arr.Get(j))
+			j++
+		}
+	}
+	if j > end {
+		for i <= mid {
+			tempArray = append(tempArray, arr.Get(i))
+			i++
+		}
+	}
+	for k := range tempArray {
+		arr.Set(k+start, tempArray[k])
+	}
+}
 
 func NewSorter() sortable.OutOfPlaceSorter {
 	return &MergeSorter{}
