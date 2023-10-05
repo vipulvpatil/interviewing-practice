763ed7e68590061e3682c6d7e4b6aee577462f7b
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-10-05 14:32:46 +0530

    restructured sort tests

--- Sept2023/golang/sorts/sorts_test.go
+++ Sept2023/golang/sorts/sorts_test.go
@@ -8,32 +8,38 @@ import (
 	"github.com/vipulvpatil/interviewing-practice/Sept2023/golang/sorts/insertion"
 	"github.com/vipulvpatil/interviewing-practice/Sept2023/golang/sorts/selection"
 	"github.com/vipulvpatil/interviewing-practice/Sept2023/golang/sorts/shell"
+	"github.com/vipulvpatil/interviewing-practice/Sept2023/golang/sorts/sortable"
 	sortableelements "github.com/vipulvpatil/interviewing-practice/Sept2023/golang/sorts/sortable_elements"
 )
 
-func Test_SelectionSort(t *testing.T) {
-	t.Run("Selection sort works", func(t *testing.T) {
-		arr := sortableelements.NewSortableInts(8, 4, 2, 6, 3, 7, 1, 9, 5)
-		selection.NewSorter().Sort(arr)
-		output := fmt.Sprint(arr)
-		assert.Equal(t, "1, 2, 3, 4, 5, 6, 7, 8, 9", output)
-	})
-}
+func Test_Sort(t *testing.T) {
+	input := []int{8, 4, 2, 6, 3, 7, 1, 9, 5}
+	expectedOutput := "1, 2, 3, 4, 5, 6, 7, 8, 9"
+	tests := []struct {
+		name   string
+		sorter sortable.Sorter
+	}{
+		{
+			name:   "Selection sort",
+			sorter: selection.NewSorter(),
+		},
+		{
+			name:   "Insertion sort",
+			sorter: insertion.NewSorter(),
+		},
+		{
+			name:   "Shell sort",
+			sorter: shell.NewSorter(),
+		},
+	}
 
-func Test_InsertionSort(t *testing.T) {
-	t.Run("Insertion sort works", func(t *testing.T) {
-		arr := sortableelements.NewSortableInts(8, 4, 2, 6, 3, 7, 1, 9, 5)
-		insertion.NewSorter().Sort(arr)
-		output := fmt.Sprint(arr)
-		assert.Equal(t, "1, 2, 3, 4, 5, 6, 7, 8, 9", output)
-	})
-}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			arr := sortableelements.NewSortableInts(input...)
+			tt.sorter.Sort(arr)
+			output := fmt.Sprint(arr)
+			assert.Equal(t, expectedOutput, output)
+		})
+	}
 
-func Test_ShellSort(t *testing.T) {
-	t.Run("Shell sort works", func(t *testing.T) {
-		arr := sortableelements.NewSortableInts(8, 4, 2, 6, 3, 7, 1, 9, 5)
-		shell.NewSorter().Sort(arr)
-		output := fmt.Sprint(arr)
-		assert.Equal(t, "1, 2, 3, 4, 5, 6, 7, 8, 9", output)
-	})
 }
