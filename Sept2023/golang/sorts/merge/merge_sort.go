2984b0524f0d4f7ca521a1c109f6036a8da70f28
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-10-05 18:03:25 +0530

    added a non recursive merge sort

--- Sept2023/golang/sorts/merge/merge_sort.go
+++ Sept2023/golang/sorts/merge/merge_sort.go
@@ -7,7 +7,18 @@ import (
 type MergeSorter struct{}
 
 func (s *MergeSorter) Sort(arr sortable.SortableOutOfPlace) {
-	MergeSort(arr, 0, arr.Len()-1)
+	for sz := 1; sz < arr.Len(); sz = sz * 2 {
+		for start := 0; start < arr.Len(); start += sz * 2 {
+			end := start + sz*2 - 1
+			if end > arr.Len()-1 {
+				end = arr.Len() - 1
+			}
+			mid := start + sz - 1
+			MergeSortBottomUp(arr, start, mid, end)
+		}
+	}
+	// OR
+	// MergeSort(arr, 0, arr.Len()-1)
 }
 
 func MergeSort(arr sortable.SortableOutOfPlace, start, end int) {
@@ -18,6 +29,40 @@ func MergeSort(arr sortable.SortableOutOfPlace, start, end int) {
 	tempArray := []any{}
 	MergeSort(arr, start, mid)
 	MergeSort(arr, mid+1, end)
+	i := start
+	j := mid + 1
+	for i <= mid && j <= end {
+		if arr.Less(i, j) {
+			tempArray = append(tempArray, arr.Get(i))
+			i++
+		} else {
+			tempArray = append(tempArray, arr.Get(j))
+			j++
+		}
+	}
+	if i > mid {
+		for j <= end {
+			tempArray = append(tempArray, arr.Get(j))
+			j++
+		}
+	}
+	if j > end {
+		for i <= mid {
+			tempArray = append(tempArray, arr.Get(i))
+			i++
+		}
+	}
+	for k := range tempArray {
+		arr.Set(k+start, tempArray[k])
+	}
+}
+
+func MergeSortBottomUp(arr sortable.SortableOutOfPlace, start, mid, end int) {
+	if end-start < 1 {
+		return
+	}
+
+	tempArray := []any{}
 	i := start
 	j := mid + 1
 	for i <= mid && j <= end {
