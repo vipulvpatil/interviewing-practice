d47c900edeb03311bc313c603104da800bb486f1
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-10-06 18:39:16 +0530

    added collinear solutions

---
+++ Sept2023/Algorithms_Princeton/week3/collinear/BruteCollinearPoints.java
@@ -0,0 +1,89 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class BruteCollinearPoints {
+  private LineSegment[] lineSegments;
+
+  public BruteCollinearPoints(Point[] points) {
+    if (points == null) {
+      throw new IllegalArgumentException();
+    }
+
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] == null) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    this.lineSegments = new LineSegment[0];
+
+    for (int i = 0; i < points.length; i++) {
+      for (int j = i + 1; j < points.length; j++) {
+        for (int k = j + 1; k < points.length; k++) {
+          for (int l = k + 1; l < points.length; l++) {
+            // StdOut.printf("%d\t%d\t%d\t%d\n", i, j, k, l);
+            Point pI = points[i];
+            Point pJ = points[j];
+            Point pK = points[k];
+            Point pL = points[l];
+            // StdOut.printf("%s\t%s\t%s\t%s\n", pI.toString(), pJ.toString(),
+            // pK.toString(), pL.toString());
+            // StdOut.printf("%f\t%f\t%f\n", pI.slopeTo(pJ), pI.slopeTo(pK),
+            // pI.slopeTo(pL));
+            if (pI.slopeTo(pJ) == pI.slopeTo(pK) &&
+                pI.slopeTo(pJ) == pI.slopeTo(pL)) {
+              int n = this.numberOfSegments();
+              LineSegment[] newLineSegments = new LineSegment[n + 1];
+              for (int x = 0; x < n; x++) {
+                newLineSegments[x] = this.lineSegments[x];
+              }
+
+              Point[] collinearPoints = { pI, pJ, pK, pL };
+              Arrays.sort(collinearPoints);
+              newLineSegments[n] = new LineSegment(collinearPoints[0], collinearPoints[3]);
+              this.lineSegments = newLineSegments;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public int numberOfSegments() {
+    return this.lineSegments.length;
+  }
+
+  public LineSegment[] segments() {
+    return this.lineSegments;
+  }
+
+  public static void main(String[] args) {
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+      int x = in.readInt();
+      int y = in.readInt();
+      points[i] = new Point(x, y);
+    }
+
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+      p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+      segment.draw();
+    }
+    StdDraw.show();
+  }
+}
---
+++ Sept2023/Algorithms_Princeton/week3/collinear/FastCollinearPoints.java
@@ -0,0 +1,129 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FastCollinearPoints {
+  private LineSegment[] lineSegments;
+
+  public FastCollinearPoints(Point[] points) {
+    if (points == null) {
+      throw new IllegalArgumentException();
+    }
+
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] == null) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    this.lineSegments = new LineSegment[0];
+
+    Arrays.sort(points);
+
+    for (int i = 0; i < points.length; i++) {
+      Point p0 = points[i];
+      Point[] otherPoints = new Point[points.length - i - 1];
+      StdOut.printf("%d\n", i);
+      StdOut.printf("%s\n", p0.toString());
+      for (int j = 0; j < points.length - i - 1; j++) {
+        otherPoints[j] = points[j + i + 1];
+      }
+
+      Arrays.sort(otherPoints, p0.slopeOrder());
+
+      Point[] collinearPoints = new Point[0];
+      for (int j = 0; j < otherPoints.length - 1; j++) {
+        StdOut.printf(" %d: %s %f", j, otherPoints[j].toString(), p0.slopeTo(otherPoints[j]));
+        StdOut.printf(" %d: %s %f", j + 1, otherPoints[j + 1].toString(), p0.slopeTo(otherPoints[j + 1]));
+        StdOut.println();
+        if (p0.slopeTo(otherPoints[j]) == p0.slopeTo(otherPoints[j + 1])) {
+          if (collinearPoints.length == 0) {
+            collinearPoints = append(collinearPoints, otherPoints[j]);
+          }
+          collinearPoints = append(collinearPoints, otherPoints[j + 1]);
+        } else {
+          if (collinearPoints.length >= 3) {
+            collinearPoints = append(collinearPoints, p0);
+            Arrays.sort(collinearPoints);
+            for (int x = 0; x < collinearPoints.length; x++) {
+              StdOut.print(collinearPoints[x].toString());
+            }
+            StdOut.println();
+            LineSegment lineSegment = new LineSegment(collinearPoints[0], collinearPoints[collinearPoints.length - 1]);
+            this.lineSegments = append(this.lineSegments, lineSegment);
+          }
+          collinearPoints = new Point[0];
+        }
+      }
+      if (collinearPoints.length >= 3) {
+        collinearPoints = append(collinearPoints, p0);
+        Arrays.sort(collinearPoints);
+        for (int x = 0; x < collinearPoints.length; x++) {
+          StdOut.print(collinearPoints[x].toString());
+        }
+        StdOut.println();
+        LineSegment lineSegment = new LineSegment(collinearPoints[0], collinearPoints[collinearPoints.length - 1]);
+        this.lineSegments = append(this.lineSegments, lineSegment);
+      }
+    }
+  }
+
+  private LineSegment[] append(LineSegment[] lineSegments, LineSegment lineSegment) {
+    int n = lineSegments.length;
+    LineSegment[] newLineSegments = new LineSegment[n + 1];
+    for (int x = 0; x < n; x++) {
+      newLineSegments[x] = lineSegments[x];
+    }
+    newLineSegments[n] = lineSegment;
+    return newLineSegments;
+  }
+
+  private Point[] append(Point[] points, Point point) {
+    int n = points.length;
+    Point[] newPoints = new Point[n + 1];
+    for (int x = 0; x < n; x++) {
+      newPoints[x] = points[x];
+    }
+    newPoints[n] = point;
+    return newPoints;
+  }
+
+  public int numberOfSegments() {
+    return this.lineSegments.length;
+  }
+
+  public LineSegment[] segments() {
+    return this.lineSegments;
+  }
+
+  public static void main(String[] args) {
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+      int x = in.readInt();
+      int y = in.readInt();
+      points[i] = new Point(x, y);
+    }
+
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+      p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    FastCollinearPoints collinear = new FastCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+      segment.draw();
+      StdOut.println(segment.toString());
+    }
+    StdDraw.show();
+  }
+}
